c26c8420451373aeb393a20c554231f0c77093af
Kuba Parol <99417488+kubaparol@users.noreply.github.com>
2024-07-22 17:51:47 +0200

    Feature/column actions (#7)
    * ft: add delete column functionality to BoardContainer
    
    * ft: add edit column functionality to BoardContainer

--- src/services/api/modules/column/index.ts
+++ src/services/api/modules/column/index.ts
@@ -1,11 +1,35 @@
 import { AbstractApiModule } from "../../helpers";
-import { CreateColumnRequest, CreateColumnResponse } from "./types";
+import {
+  CreateColumnRequest,
+  CreateColumnResponse,
+  DeleteColumnRequest,
+  DeleteColumnResponse,
+  EditColumnRequest,
+} from "./types";
 
 export class ColumnModule extends AbstractApiModule {
   async create(data: CreateColumnRequest) {
     return this.fetcher<CreateColumnResponse>("column", {
       method: "POST",
       data,
+    }).then((res) => res.data);
+  }
+
+  async edit(data: EditColumnRequest) {
+    const { id, ...rest } = data;
+
+    return this.fetcher<EditColumnRequest>(`column/${id}`, {
+      method: "PATCH",
+      data: rest,
+    }).then((res) => res.data);
+  }
+
+  async delete(data: DeleteColumnRequest) {
+    const { id, ...rest } = data;
+
+    return this.fetcher<DeleteColumnResponse>(`column/${id}`, {
+      method: "DELETE",
+      data: rest,
     }).then((res) => res.data);
   }
 }
--- src/services/api/modules/column/types.ts
+++ src/services/api/modules/column/types.ts
@@ -15,3 +15,25 @@ export interface CreateColumnResponse extends CreateColumnRequest {
   id: string;
 }
 /**/
+
+/* Delete */
+export interface EditColumnRequest extends Partial<CreateColumnRequest> {
+  id: string;
+}
+
+export interface EditColumnResponse extends Column {
+  boardId: string;
+}
+/**/
+
+/* Delete */
+export interface DeleteColumnRequest {
+  id: string;
+}
+
+export interface DeleteColumnResponse {
+  id: string;
+  name: string;
+  boardId: string;
+}
+/**/
