3612e24ee192cb4f70f2c20eddfa341b7c0ca75f
kubaparol <kubciop33@gmail.com>
2024-10-25 11:36:38 +0200

    ft: add link to home page in header component

--- src/components/base/Header.tsx
+++ src/components/base/Header.tsx
@@ -1,6 +1,8 @@
 import { cn } from "@/utils";
 import { ComponentPropsWithoutRef, FC } from "react";
 import { Logo } from "./Logo";
+import { AppUrls } from "@/router/urls";
+import { Link } from "react-router-dom";
 
 export interface HeaderProps extends ComponentPropsWithoutRef<"header"> {}
 
@@ -9,7 +11,9 @@ export const Header: FC<HeaderProps> = (props) => {
 
   return (
     <header {...rest} className={cn("wrapper", className)}>
-      <Logo />
+      <Link to={AppUrls.home}>
+        <Logo />
+      </Link>
     </header>
   );
 };
